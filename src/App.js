dcdf68711b0e4ce488bb22fa66ae261e40e3e04b
박지은 <jepark@onthelive.kr>
2023-08-17 16:54:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {CssBaseline} from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import ErrorPage from "./pages/home/Error";
+import HomePage from "./pages/home/Home";
+import SamplePage from "./pages/home/sample/Sample";
+import Studio from "./pages/home/studio/Studio";
+import Streaming from "./pages/viewer/streaming/Streaming";
+import Watching from "./pages/viewer/watching/Watching";
+
+export const topBarHeight = 54;
+export const drawerOpenWidth = 220;
+export const drawerCloseWidth = 70;
+
+const theme = createTheme({
+
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/sample',
+    element: <SamplePage />,
+  },
+  {
+    path: '/studio',
+    element: <Studio />,
+  },
+  {
+    path: '/streaming',
+    element: <Streaming />,
+  },
+  {
+    path: '/watching',
+    element: <Watching />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
   );
 }
 
